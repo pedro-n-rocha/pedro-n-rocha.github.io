54edc96fd4e8467a0f0eeec6f5e99fa459942bc5
Pedro Rocha <pedro-n-rocha@live.com>
2017-07-25 16:50:24 -0400

    init test

---
+++ js/init.js
@@ -0,0 +1,5 @@
+import {Sandbox, SandboxRegistry, SandboxType} from 'sandbox'
+import MiniBus from 'minibus';
+
+console.log('Sandbox: ', Sandbox, SandboxRegistry);
+console.log('MiniBus: ', MiniBus);
